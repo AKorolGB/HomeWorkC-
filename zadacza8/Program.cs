ac39f7e8b221989c9a1b8d1cb3eaad12bd0ea252
AKorolGB <Alexander94.13@mail.ru>
2023-03-11 22:52:07 +0100

    Задача 8

---
+++ zadacza8/Program.cs
@@ -0,0 +1,18 @@
+﻿// Console.Clear();
+// Console.Write("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine()); 
+//  for (int a = 1; a <= n; a++) 
+//  if (a % 2 == 0)
+//   Console.WriteLine(a);
+          
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine()); 
+int a = 1;
+ while ( a <= n) {
+ if (a % 2 == 0)
+  Console.WriteLine(a);
+  a++;
+ }
+
