c5e27645b7f7623c7178b8e162bdedcedfc21075
AKorolGB <Alexander94.13@mail.ru>
2023-03-12 19:19:36 +0100

    Задача 13

---
+++ zadacza13/Program.cs
@@ -0,0 +1,15 @@
+﻿// Console.Clear();
+// Console.WriteLine("введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// string s = Convert.ToString(n);
+// if (n/100 < 1 )
+// Console.WriteLine("третей цифры нет");
+// else
+// Console.WriteLine(s[2]);
+
+Console.Clear();
+Console.WriteLine("введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string s = Convert.ToString(n);
+
+Console.WriteLine(s[2]);
