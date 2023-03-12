3ef07836d8dc8870864d87fbef98ce875157ba5c
AKorolGB <Alexander94.13@mail.ru>
2023-03-12 18:27:52 +0100

    Задача 10

---
+++ zadacza10/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.Clear();
+Console.WriteLine("Введите трехзначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n<100 || n>1000)
+{
+Console.WriteLine("Ошибка!!\nВведите трехзначное число: ");
+n = Convert.ToInt32(Console.ReadLine());
+}
+int m = n%100;
+int s = m/10;
+Console.WriteLine(s);
+
+
