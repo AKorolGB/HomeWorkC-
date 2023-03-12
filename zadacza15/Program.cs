0b6924bc0e048e7692828d276a304ad71a054cba
AKorolGB <Alexander94.13@mail.ru>
2023-03-12 19:39:33 +0100

    Задача 15

---
+++ zadacza15/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.Clear();
+Console.WriteLine("Введите цифру дня недел:\n1-Понедельник\n2-Вторник\n3-Среда\n4-Четверг\n5-Пятница\n6-Суббота\n7-Воскресенье ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n<1 || n>7) {
+Console.WriteLine("Ошибка!!!\nВведите цифру дня недел:\n1-Понедельник\n2-Вторник\n3-Среда\n4-Четверг\n5-Пятница\n6-Суббота\n7-Воскресенье ");
+n = Convert.ToInt32(Console.ReadLine());
+}
+if (n == 6 || n == 7)
+Console.WriteLine("Да");
+else
+Console.WriteLine("Нет");
