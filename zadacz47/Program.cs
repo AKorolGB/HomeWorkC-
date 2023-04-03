e0637065d666adfd7f7093bae001bab6c7505f26
AKorolGB <Alexander94.13@mail.ru>
2023-04-03 17:39:08 +0200

    zadacza47a

--- zadacz47/Program.cs
+++ zadacz47/Program.cs
@@ -1,33 +1,4 @@
-﻿// //  Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
-
-// void InputMatrix(int[,] matrix)
-// {
-//   for(int i = 0; i < matrix.GetLength(0); i++)
-//   {
-//     for(int j = 0; i < matrix.GetLength(1); j++)
-//     matrix[i,j]= new Random().Next(0,10);
-//   }
-// }
-// //  matrix[i,j]= Math.Round(new Random().NextDouble(), 2);
-// void PrintMatrix(int[,] matrix)
-// {
-//   for (int i = 0; i < matrix.GetLength(0); i++)
-//   {
-//     for(int j=0; j < matrix.GetLength(1); j++)
-// Console.Write($"{matrix[i,j]} \t" );
-//     Console.WriteLine();
-//   }
-// }
-
-
-
-// Console.Clear();
-// Console.Write("Введите размерность массива: ");
-// // int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
-// int[] size = Console.ReadLine()!.Split().Select( x=> int.Parse(x)).ToArray();
-// int[,]matrix =new int[size[0], size[1]];
-// InputMatrix(matrix);
-// PrintMatrix(matrix);
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
 
 
 void InputMatrix(double[,] matrix)
