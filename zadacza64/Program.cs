6e66b5d61ee1abd129b3297d88443562c38d993c
AKorolGB <Alexander94.13@mail.ru>
2023-04-07 20:34:00 +0200

    Zadacza64

--- zadacza64/Program.cs
+++ zadacza64/Program.cs
@@ -1,21 +1,29 @@
 ﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
 
-string f(int n , int a)
+// string f(int n , int a)
+// {
+//   if (n == a)
+//     return $" {a}";
+//   return $" {n}" + f(n - 1,a)  ;
+// }
+
+string f(int n )
 {
-  if (n == a)
-    return $" {a}";
-  return $" {n}" + f(n - 1,a)  ;
+  if (n == 1)
+    return $" 1";
+  return $" {n}" + f(n - 1)  ;
 }
+
 
 
 Console.Clear();
 Console.WriteLine("Введите значение N:");;
 int n = int.Parse(Console.ReadLine()!);
-Console.WriteLine("Введите значение A :");;
-int a = int.Parse(Console.ReadLine()!);
-
-if (n > a)
-Console.WriteLine(f(n,a));
-else
-  Console.WriteLine(f(a,n));
+// Console.WriteLine("Введите значение A :");;
+// int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine(f(n));
+// if (n > a)
+// Console.WriteLine(f(n,a));
+// else
+//   Console.WriteLine(f(a,n));
 
