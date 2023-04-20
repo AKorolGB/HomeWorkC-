9228337440e7bf831c159fef5a3ba6bbfb2c5bac
AKorolGB <Alexander94.13@mail.ru>
2023-04-20 16:01:13 +0200

    kont

---
+++ kont/Program.cs
@@ -0,0 +1,58 @@
+﻿void ImputArray(string[] array)
+{
+for (int i = 0; i < array.Length; i++)
+{
+Console.Write($"Введите {i+1} элемент массива: ");
+  array[i]= Console.ReadLine();
+}
+}
+
+
+int longbArray(string[] array)
+{
+int longb = 0;
+for (int i = 0; i < array.Length; i++)
+if (array[i].Length <= 3){
+// b[sum] = a[i];
+longb =longb +1;
+}
+   return longb;
+}
+
+void newArray(string[] array)
+{
+int longb = 0; 
+string[] barray = new string[longb];
+for (int i = 0; i < array.Length; i++)
+{
+if (array[i].Length <= 3)
+{
+longb =longb +1;
+}
+}
+int f = 0;
+for (int i = 0; i < array.Length; i++)
+if (array[i].Length <= 3)
+{
+barray[f] = array[i];
+f = f +1;
+}
+}
+
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string[] array = new string[n];
+int longb = 0; 
+// for (int i = 0; i < array.Length; i++){
+// Console.Write($"Введите {i+1} элемент массива: ");
+//   a[i]= Console.ReadLine();
+// }
+ImputArray(array);
+longbArray(array);
+Console.WriteLine(longbArray(array));
+// string[] barray = new string[longb];
+newArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+Console.WriteLine($"Новый  массив: [{string.Join(", ", newArray(array))}]");
